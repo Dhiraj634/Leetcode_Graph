396f7611aa42367b8b224e0a975ab9bef1c4e3cb
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-12-29 17:10:58 +0530

    src/FindCenterOfStarGraph.java

--- src/FindCenterOfStarGraph.java
+++ src/FindCenterOfStarGraph.java
@@ -1,5 +1,30 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 public class FindCenterOfStarGraph {
 	public int findCenter(int[][] edges) {
-		return 0;
+		Map<Integer, HashSet<Integer>> graph = new HashMap<>();
+		for(int[] edge:edges){
+			graph.putIfAbsent(edge[0],new HashSet<>());
+			graph.putIfAbsent(edge[1],new HashSet<>());
+			graph.get(edge[0]).add(edge[1]);
+			graph.get(edge[1]).add(edge[0]);
+		}
+		Queue<Integer> queue = new LinkedList<>();
+		for(int num: graph.keySet()){
+			if(graph.get(num).size() == 1) queue.add(num);
+		}
+		while(queue.size() > 1){
+			int node = queue.poll();
+			for(int connectedNode : graph.get(node)){
+				graph.get(connectedNode).remove(node);
+				if(graph.get(connectedNode).size() ==  1)
+					queue.add(connectedNode);
+			}
+		}
+		 return queue.poll();
 	}
 }
