a8af4d9a9e0e5c6bd89f430c3c05d3bc12c971ac
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-12-29 20:10:30 +0530

    Optimized Find the Town Judge

--- src/FindTheTownJudge.java
+++ src/FindTheTownJudge.java
@@ -1,24 +1,17 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-
 public class FindTheTownJudge {
 	public int findJudge(int n, int[][] trust) {
 		if(n==1) return 1;
-		HashMap<Integer, HashSet<Integer>> trusts = new HashMap<>();
-		HashMap<Integer, HashSet<Integer>> trustedBy = new HashMap<>();
+		int[] trusts = new int[n+1];
+		int[] trustedBy = new int[n+1];
 		for(int[] temp: trust){
-			trusts.putIfAbsent(temp[0], new HashSet<>());
-			trustedBy.putIfAbsent(temp[1], new HashSet<>());
-			trusts.get(temp[0]).add(temp[1]);
-			trustedBy.get(temp[1]).add(temp[0]);
+			trustedBy[temp[1]]++;
+			trusts[temp[0]]++;
 		}
-		ArrayList<Integer> suspect = new ArrayList<>();
 		for(int i=1;i<=n;i++){
-			if(!trusts.containsKey(i)) suspect.add(i);
-		}
-		for(int judge: suspect){
-			if(trustedBy.containsKey(judge) && trustedBy.get(judge).size() == n-1) return judge;
+			if(trusts[i]==0 && trustedBy[i] == n-1)
+			{
+				return i;
+			}
 		}
 		return -1;
 	}
